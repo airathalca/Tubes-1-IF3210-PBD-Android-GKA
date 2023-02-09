c86984fa8b34d4c707c509f29312bc917a0e93c6
Aira Thalca <13520101@std.stei.itb.ac.id>
2023-02-10 01:23:14 +0700

    create branch page

--- app/src/main/java/com/example/majika/ui/location/Adapter.kt
+++ app/src/main/java/com/example/majika/ui/location/Adapter.kt
@@ -1,21 +1,25 @@
 package com.example.majika.ui.location
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.example.majika.databinding.RecyclerRowBranchBinding
+import com.example.majika.databinding.BranchCardBinding
 import com.example.majika.models.BranchData
-import kotlinx.android.synthetic.main.recycler_row_branch.view.*
+import kotlinx.android.synthetic.main.branch_card.view.*
+
 
 class BranchAdapter: RecyclerView.Adapter<BranchAdapter.Holder>(){
     private var branchList = arrayListOf<BranchData>()
     class Holder(val view: View) : RecyclerView.ViewHolder(view)
 
-    private lateinit var binding: RecyclerRowBranchBinding
+    private lateinit var binding: BranchCardBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        binding = RecyclerRowBranchBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        binding = BranchCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return Holder(binding.root)
     }
 
@@ -24,11 +28,21 @@ class BranchAdapter: RecyclerView.Adapter<BranchAdapter.Holder>(){
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.view.branch_list_name.text = branchList[position].name
+        holder.view.branch_name.text = branchList[position].name
+        holder.view.branch_address.text = branchList[position].address
+        holder.view.branch_contact.text = branchList[position].contact_person.plus(" - ").plus(branchList[position].phone_number)
+        holder.view.button_maps.setOnClickListener {
+            val gmmIntentUri = Uri.parse("geo:${branchList[position].latitude},${branchList[position].longitude}?q=${branchList[position].latitude},${branchList[position].longitude}(${branchList[position].name})")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(holder.view.context, mapIntent, null)
+        }
     }
 
     fun setData(newBranchList: ArrayList<BranchData>){
-        branchList = newBranchList
+        var sortedList = newBranchList.sortedWith(compareBy { it.name })
+        branchList.clear()
+        branchList.addAll(sortedList)
         notifyDataSetChanged()
     }
 }
