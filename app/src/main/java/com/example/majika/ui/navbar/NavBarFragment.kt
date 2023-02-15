59539236fc6855335c5a042d519f6a6fb9a043b4
Aira Thalca <13520101@std.stei.itb.ac.id>
2023-02-15 12:03:33 +0700

    fix navbar

--- app/src/main/java/com/example/majika/ui/navbar/NavBarFragment.kt
+++ app/src/main/java/com/example/majika/ui/navbar/NavBarFragment.kt
@@ -25,18 +25,34 @@ class NavBarFragment : Fragment() {
         val navController = requireActivity().findNavController(R.id.nav_host_fragment_activity_main)
 
         binding.navigationTwibbon.setOnClickListener {
+            binding.navigationTwibbon.isSelected = true
+            binding.navigationLocation.isSelected = false
+            binding.navigationFoodBank.isSelected = false
+            binding.navigationShoppingCart.isSelected = false
             navController.navigate(R.id.navigation_twibbon)
         }
 
         binding.navigationLocation.setOnClickListener {
+            binding.navigationTwibbon.isSelected = false
+            binding.navigationLocation.isSelected = true
+            binding.navigationFoodBank.isSelected = false
+            binding.navigationShoppingCart.isSelected = false
             navController.navigate(R.id.navigation_location)
         }
 
         binding.navigationFoodBank.setOnClickListener {
+            binding.navigationTwibbon.isSelected = false
+            binding.navigationLocation.isSelected = false
+            binding.navigationFoodBank.isSelected = true
+            binding.navigationShoppingCart.isSelected = false
             navController.navigate(R.id.navigation_food_bank)
         }
 
         binding.navigationShoppingCart.setOnClickListener {
+            binding.navigationTwibbon.isSelected = false
+            binding.navigationLocation.isSelected = false
+            binding.navigationFoodBank.isSelected = false
+            binding.navigationShoppingCart.isSelected = true
             navController.navigate(R.id.navigation_shopping_cart)
         }
 
