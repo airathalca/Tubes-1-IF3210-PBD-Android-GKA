62d4a43e9d3db5fee62526c2c19e65dda7151965
Gagas Praharsa Bahar <gagaspbahar@gmail.com>
2023-02-23 23:43:18 +0700

    add splash screen and app logo

---
+++ app/src/main/java/com/example/majika/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.example.majika
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 3000) // 3000 milliseconds delay before starting the MainActivity
+    }
+}
